54ca4173b8c52fd56f2a1432e69e12f559a166e6
chataignault <leonchat@orange.fr>
2025-11-15 16:30:45 +0000

    update similarity

--- similarity/__init__.py
+++ similarity/__init__.py
@@ -3,3 +3,12 @@
 Package pour le calcul de similarité entre embeddings.
 """
 
+from .similarity import (
+    cosine_similarity,
+    find_similar_sounds
+)
+
+__all__ = [
+    'cosine_similarity',
+    'find_similar_sounds'
+]

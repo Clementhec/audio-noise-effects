8267c112aad83976054b1e46b055846a1486be24
chataignault <leonchat@orange.fr>
2025-12-06 12:38:47 +0000

    sounds: rename scripts

--- sounds/elevenlabs_utils.py
+++ sounds/elevenlabs_utils.py
@@ -1,7 +1,7 @@
 import os
 from pathlib import Path
 from dotenv import load_dotenv
-from elevenlabs.client import ElevenLabs
+from elevenlabs import ElevenLabs
 from typing import Optional
 import hashlib
 
